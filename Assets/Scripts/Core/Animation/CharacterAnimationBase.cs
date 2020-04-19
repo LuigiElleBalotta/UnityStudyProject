495c0f2181d4c3fecfc2c7621c4c3a520a85f4c4
Luigi Balotta <luigi@thebbsway.com>
2020-04-19 21:45:53 +0200

    init

---
+++ Assets/Scripts/Core/Animation/CharacterAnimationBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationBase : MonoBehaviour
+{
+    public enum AnimationStates
+    {
+        Stand = 0,
+        Walk = 1,
+        Run = 2,
+        WalkBack = 3,
+        RotateLeft = 4,
+        RotateRight = 5
+    }
+
+    public AnimationStates currentState;
+
+    [SerializeField] private Animator p_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CharacterAnimation()
+    {
+        Animator.SetInteger("p_currentState", (int)AnimationStates.Stand);
+    }
+
+    public Animator Animator
+    {
+        get
+        {
+            if (p_Animator == null)
+                p_Animator = GetComponentInChildren<Animator>();
+
+            return p_Animator;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Movement/CharacterController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CharacterAnimationBase;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 40;
+    float rotSpeed = 80;
+    public float gravity = 8;
+
+    Vector3 moveDir = Vector3.zero;
+
+    //UnityEngine.CharacterController controller;
+    //Transform playerContainer;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controller = GetComponentInChildren<UnityEngine.CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+        //playerContainer = GameObject.FindGameObjectWithTag("PlayerContainer").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+            moveDir = Vector3.forward;
+            moveDir *= speed;
+            anim.SetInteger("p_currentState", (int)AnimationStates.Run);
+
+            transform.Translate(moveDir, Space.World);
+            
+        }
+        else if( Input.GetKey(KeyCode.S))
+        {
+            moveDir = Vector3.back;
+            moveDir *= speed;
+            anim.SetInteger("p_currentState", (int)AnimationStates.WalkBack);
+
+            transform.Translate(moveDir, Space.World);
+        }
+        else if( Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0, Space.Self);
+            anim.SetInteger("p_currentState", (int)AnimationStates.RotateRight);
+        }
+        else if( Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0, Space.Self);
+            anim.SetInteger("p_currentState", (int)AnimationStates.RotateLeft);
+        }
+        else
+        {
+            anim.SetInteger("p_currentState", (int)AnimationStates.Stand);
+            moveDir = Vector3.zero;
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        //controller.Move(moveDir);
+
+        //playerContainer.transform.position = transform.position;
+
+
+
+    }
+}
